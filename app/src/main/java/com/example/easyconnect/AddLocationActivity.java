ae30811a4607f060d73c4701051ce8075b4faae8
Rick <Rick@DESKTOP-NTB5OVF>
2025-02-02 13:11:22 +0800

    ADDED CLICK FUNCTION

--- app/src/main/java/com/example/easyconnect/AddLocationActivity.java
+++ app/src/main/java/com/example/easyconnect/AddLocationActivity.java
@@ -1,9 +1,15 @@
 package com.example.easyconnect;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +26,15 @@ public class AddLocationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void backToMainActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void showInput(View view){
+        ConstraintLayout constraint = findViewById(R.id.inputConstraint);
+        constraint.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/com/example/easyconnect/MainActivity.java
+++ app/src/main/java/com/example/easyconnect/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.easyconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void GoToAddLocationActivity(View view){
+        Intent intent = new Intent(MainActivity.this, AddLocationActivity.class);
+        startActivity(intent);
     }
 }
