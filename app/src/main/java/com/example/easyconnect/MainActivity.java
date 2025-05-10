755a260352ac830c79e58d0f2aa15b15381d6e82
jerick octaviano <jerickoctaviano01@gmail.com>
2025-05-10 15:31:09 +0800

    LATEST

--- app/src/main/java/com/example/easyconnect/MainActivity.java
+++ app/src/main/java/com/example/easyconnect/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
@@ -34,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        ConstraintLayout editContainer = findViewById(R.id.editContainer);
+        editContainer.setVisibility(View.GONE);
 
         List<String> allLocations = SharedDataModel.getInstance().getLocations();
         for (String mapName : allLocations) {
